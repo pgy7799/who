929f8c40f523a793e5c9744190f1718293ab69a3
pgy7799 <pgy7799@gmail.com>
2019-07-31 18:41:37 +0900

    first commit

---
+++ app_blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from .models import Blog
+
+def home(request):
+    blogs = Blog.objects
+
+    return render(request, 'home.html', {'note':blogs})
